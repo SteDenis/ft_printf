e2d565a4df9aca132629a31e75eb49c99ae0b3c3
Steve Denis <stdenis@macbook-air-de-steve.home>
2019-03-03 21:38:45 +0100

    Update

--- main_test/test4.c
+++ main_test/test4.c
@@ -32,7 +32,7 @@ void double_to_bits(void *var)
 {
 	int		byte;
 	unsigned char *tmp = (unsigned char *)var;
-	int i = sizeof(int);
+	int i = sizeof(unsigned long long);
 	int j;
 
 	while (i--)
@@ -48,43 +48,63 @@ void double_to_bits(void *var)
 	}
 }
 
+long double ft_frexpl(long double value, int *exp)
+{
+	union {
+		long double val;
+		struct {
+			unsigned mantissa2 : 32;
+			unsigned mantissa1 : 32;
+			unsigned exponent : 15;
+			unsigned sign :  1;
+			unsigned empty : 16;
+		} binary;
+	} uniondbl;
+	uniondbl.val = value;
+	*exp = uniondbl.binary.exponent - 16383;
+	uniondbl.binary.exponent = 16383;
+	return (uniondbl.val);
+}
+
 int 	main(void)
 {
 	char buf[999999];
-	double c = LDBL_MIN;
-	long double y =  LDBL_MIN;
-	printf("MANT_DIG = %d, MAX_EXP = %d  %d\n", LDBL_MANT_DIG, LDBL_MAX_EXP, 3U);
-	//uint32_t big[(LDBL_MANT_DIG+28)/29 + 1          // mantissa expansion
-	//			 + (LDBL_MAX_EXP+LDBL_MANT_DIG+28+8)/9]; // exponent expansion
+	double c = 49313.1234197;
+	long double y =  (long double)c;
 	uint32_t big[2000];
 	uint32_t *a, *d, *r, *z;
 	int e2=0, e, i, j, l;
-	int			p = 10;
+	int			p = 15;
 
 	if (y < 0)
 		y=-y;
-	y = frexpl(y, &e2) * 2;
+	y = ft_frexpl(y, &e2) * 2;
 	double_to_bits(&e2);
-	printf("\nfrexp() makes %Lf * 2^%d\n", y, e2);
-	if (y)
-		e2--;
+	printf("\nfrexp() makes %Lf * 2^%d  %d\n", y, e2, FLT_RADIX);
+	//if (y)
+		//e2--;
 	double_to_bits(&e2);
 	printf("\n");
 	if (y)
 	{
 		y *= 0x1p28;
-		e2 -= 28;
+		e2 -= 29;
 	}
+	printf("\nfrexp() makes %Lf * 2^%d  %d\n", y, e2, FLT_RADIX);
 	double_to_bits(&e2);
 	printf("\n");
-	printf("frexp() makes %Lf * 2^%d\n", y, e2);
+	printf("BID ? %lu, %d\n",+sizeof(big)/sizeof(*big) - LDBL_MANT_DIG - 1, (LDBL_MANT_DIG+28)/29 + 1+ (LDBL_MAX_EXP+LDBL_MANT_DIG+28+8)/9);
 	if (e2<0) a=r=z=big;
-	else a=r=z=big+sizeof(big)/sizeof(*big) - LDBL_MANT_DIG - 1;
+	else a=r=z=big+1999; //sizeof(big)/sizeof(*big) - LDBL_MANT_DIG - 1;
 
+	long double kk;
 	do {
 		*z = y;
-		y = 1000000000*(y-*z++);
+		kk = y-*z++;
+		y = 1000000000*(kk);
 	} while (y);
+
+	// mantissa * 2^29 ou 2^exponent if exponent < 29
 	while (e2>0) {
 		uint32_t carry=0;
 		int sh=MIN(29,e2);
@@ -97,12 +117,16 @@ int 	main(void)
 		while (z>a && !z[-1]) z--;
 		e2-=sh;
 	}
+
+	//(1<<sh)-1 = 511 (2^0+2^1+2^2+2^4+...2^8) or 2^9 - 1
 	while (e2<0) {
 		uint32_t carry=0, *b;
 		int sh=MIN(9,-e2), need=1+(p+LDBL_MANT_DIG/3U+8)/9;
 		for (d=a; d<z; d++) {
 			uint32_t rm = *d & (1<<sh)-1;
+			// *d = *d * 2^-9 + reste;
 			*d = (*d>>sh) + carry;
+			// 2^-9 * (frac * 2^9 - 1);
 			carry = (1000000000>>sh) * rm;
 		}
 		if (!*a) a++;
@@ -113,21 +137,18 @@ int 	main(void)
 		e2+=sh;
 	}
 
-	if (a<z) for (i=10, e=9*(r-a); *a>=i; i*=10, e++);
-	else e=0;
-
-
 	char t = 'f';
-	j = p - ((t)!='f')*e - ((t)=='g' && p);
+	uint32_t x;
+	/*j =10;
 	if (j < 9*(z-r-1)) {
 		uint32_t x;
-		/* We avoid C's broken division of negative numbers */
+		//We avoid C's broken division of negative numbers
 		d = r + 1 + ((j+9*LDBL_MAX_EXP)/9 - LDBL_MAX_EXP);
 		j += 9*LDBL_MAX_EXP;
 		j %= 9;
 		for (i=10, j++; j<9; i*=10, j++);
 		x = *d % i;
-		/* Are there any significant digits past j? */
+		// Are there any significant digits past j?*/
 		if (x || d+1!=z) {
 			long double round = 2/LDBL_EPSILON;
 			long double small;
@@ -139,7 +160,7 @@ int 	main(void)
 			round*=-1;
 			small*=-1;
 			*d -= x;
-			/* Decide whether to round by probing round+small */
+			// Decide whether to round by probing round+small
 			if (round+small != round) {
 				*d = *d + i;
 				while (*d > 999999999) {
@@ -151,14 +172,10 @@ int 	main(void)
 			}
 		}
 		if (z>d+1) z=d+1;
-	}
-
+//	}
 
 
 	for (; z>a && !z[-1]; z--);
-
-	if (e > INT_MAX-l) return -1;
-	if (e>0) l+=e;
 
 	if (a>r) a=r;
 	for (d=a; d<=r; d++) {
@@ -169,11 +186,10 @@ int 	main(void)
 		printf("%s", s);
 	}
 	printf(".");
-	//if (p || (fl&ALT_FORM)) out(f, ".", 1);
 	for (; d<z && p>0; d++, p-=9) {
 		char *s = fmt_u(*d, buf+9);
 		while (s>buf) *--s='0';
 		printf("%s", s);
 	}
-	//printf("\n\n\n%.18000Lf", LDBL_MIN);
+	printf("\n%d\n\n%.10f\n %d",p, c, 1+(p+LDBL_MANT_DIG/3U+8)/9);
 }
